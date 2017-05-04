e5e5cd62cadd5a87c04b4f18eef61312503d1b16
Wayne Mok <mok@aclanyc.com>
2017-05-04 02:53:19 +0000

    Random

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
-
-
-
+        var ScriptEd = ["Zach", "Justin", "Aaron", "Alyxe", "Julia"];
+        var Students = ["Zenebou", "Jennifer", "Kevin", "Darleny", "Bradley"];
+    $("#studentButton").click(function() {
+        var randomStudents = Math.floor(Math.random()*Students.length);
+    $("#studentDisplay").append(Students[randomStudents]);
 });
+    $("#teacherButton").click(function() {
+        var randomScriptEd = Math.floor(Math.random()*ScriptEd.length);
+    $("#teacherDisplay").append(ScriptEd[randomScriptEd]);
+});
+})
